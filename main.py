3b20e096df4f674118e070d0784b93b1aa285e85
Steve <mas.ste@gmail.com>
2024-03-10 14:45:24 +0100

    Create csv category all at once. Get infos without using dictionary.

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import requests
 import csv
 import os
 import re
+import time
 from bs4 import BeautifulSoup
 from datetime import datetime
-
 
 """ With BeautifulSoup library, get informations from books.toscrape.com like book titles, prices, ratings, 
 descriptions and many more.
@@ -24,28 +24,14 @@ CSV_HEADER = ["product_page_url", "universal_ product_code (upc)", "title", "pri
               "price_excluding_tax", "number_available", "product_description", "category", "review_rating",
               "image_url"]
 SAVE_PATH = "Outputs"
-
-
-def menu():
-    """ ask user for images scrap of not """
-
-    while True:
-        menu_choice = input("Want to get images ? y/n ")
-        if menu_choice == "n":
-            scrap_images = False
-            break
-        elif menu_choice == "y":
-            scrap_images = True
-            break
-
-    return scrap_images
+TODAY_DATE = datetime.now().strftime("%d_%m_%Y")
 
 
 def get_categories():
     """ from domain url, get a list of categories urls """
 
     print("Scraping in progress, take a coffee break...")
-    categories_urls_list = []
+    books_count = 0
     response = requests.get(URL_DOMAIN)
     categories_soup = BeautifulSoup(response.text, "html.parser")
 
@@ -55,7 +41,9 @@ def get_categories():
         categories_list.append(a["href"])
 
     for category in categories_list:
+
         if "category/books/" in category:
+            categories_urls_list = []
             url_category = "https://books.toscrape.com/" + category
 
             # Check if there is a page2 on category
@@ -63,22 +51,24 @@ def get_categories():
             while category:
                 next_page_category = url_category.replace("index.html", f"page-{n}.html")
                 try:
-                    response_nest_page = requests.get(next_page_category)
-                    response_nest_page.raise_for_status()
+                    response_next_page = requests.get(next_page_category)
+                    response_next_page.raise_for_status()
                     categories_urls_list.append(next_page_category)
                     n += 1
                 except:
+                    category_books_datas = []
                     break
 
             categories_urls_list.append(url_category)
 
-    return categories_urls_list
+            get_soup(books_count, category_books_datas, categories_urls_list)
+            create_csv(category_books_datas)
+    print(f"Succefully scraped ")
 
 
-def get_soup(scrap_images, categories_urls_list):
+def get_soup(books_count, category_books_datas, categories_urls_list):
     """ from categories urls list get soup of all the categories pages """
 
-    book_quantity = 0
     urls_list_length = len(categories_urls_list)
     for i in range(urls_list_length):
         responses = requests.get(categories_urls_list[i])
@@ -87,46 +77,34 @@ def get_soup(scrap_images, categories_urls_list):
         # Find and get the url of the book in the "h3" tag
         liste_h3 = soup_book_in_cat.find_all("h3")
         for h3 in liste_h3:
-            book_quantity += 1
+            books_count += 1
             a_h3 = h3.find("a")
             book_url = "https://books.toscrape.com/catalogue" + a_h3.get("href").replace("../../..", "")
 
             response = requests.get(book_url)
             soup_book = BeautifulSoup(response.text, "html.parser")
 
-            get_infos(scrap_images, book_url, soup_book)
+            get_infos(category_books_datas, book_url, soup_book)
 
-    print(f"Successfully scraped {book_quantity} books!")
+    end_time = time.time()
+
+    print(f"Successfully scraped {books_count} books in {round((end_time - start_time) / 60, 2)} minutes!")
 
 
-def get_infos(scrap_images, book_url, soup_book):
+def get_infos(category_books_datas, book_url, soup_book):
     """ from soup of all the categories pages get infos for one book """
 
-    data_book_dictionary = {}
-    trs_in_soup = soup_book.find_all("tr")
-    trs_in_soup_lenght = len(trs_in_soup)
-
-    for j in range(trs_in_soup_lenght):
-        cell1 = trs_in_soup[j].find("th").text
-        cell2 = trs_in_soup[j].find("td").text
-        data_book_dictionary[cell1] = cell2
-
     # UPC
-    upc = data_book_dictionary["UPC"]
+    upc = soup_book.find_all("td")[0].text
 
     # PRICE WT TAX
-    tax_price = data_book_dictionary["Price (incl. tax)"].replace("Â£", "")
+    tax_price = soup_book.find_all("td")[2].text.replace("Â£", "")
 
     # PRICE WITHOUT TAX
-    price = data_book_dictionary["Price (excl. tax)"].replace("Â£", "")
+    price = soup_book.find_all("td")[3].text.replace("Â£", "")
 
     # COUNT
-    count_text = data_book_dictionary["Availability"]
-    count = ""
-    # extract int from "In stock (XX available)" text
-    for k in count_text:
-        if k.isdigit():
-            count = f"{count}{k}"
+    count = re.search(r'\d+', soup_book.find_all("td")[5].text).group()
 
     # REVIEWS RATING
     p_soup = soup_book.find_all("p")[2]
@@ -159,51 +137,59 @@ def get_infos(scrap_images, book_url, soup_book):
     category_name = category_ul.find_all("a")[2].text
 
     # TITRE
-    li_title = category_ul.find_all("li")
+    li_title = category_ul.find_all("li")[3].text
     # Reformat title name to prevent forbidden characters while file writing
-    title = re.sub(r'[\\/*?:"<>|]', "", li_title[3].text)
+    title = re.sub(r'[\\/*?:"<>|]', "", li_title)
 
     book_datas = [book_url, upc, title, tax_price, price, count, description, category_name, reviews,
                   url_image]
 
-    create_csv(scrap_images, book_datas)
-
-    return book_datas
-
-
-def create_csv(scrap_images, book_datas):
-    """ create csv output file with date & hour """
-
-    today_date = datetime.now().strftime("%d_%m_%Y")
-    # Check if directory already exists
-    category_path = f"{SAVE_PATH}/extracted_data_{today_date}/{book_datas[7]}"
-    if not os.path.exists(category_path):
-        os.makedirs(category_path)
-
-        # Create csv output file with headers
-        with open(os.path.join(category_path, "Output.csv"), "w", newline="") as output:
-            write = csv.writer(output)
-            write.writerow(CSV_HEADER)
-
-    # Add book line to the csv output file
-    with open(os.path.join(category_path, "Output.csv"), "a", newline="", encoding="utf-8") as output:
-        write = csv.writer(output)
-        write.writerow(book_datas)
-
-    # If user wants to save images
     if scrap_images:
-        response = requests.get(book_datas[-1])
+
         if not os.path.exists(os.path.join(SAVE_PATH, "books_images", f"{book_datas[7]}")):
             os.makedirs(os.path.join(SAVE_PATH, "books_images", f"{book_datas[7]}"))
 
-        # Limit characters numbers for file name
+        response = requests.get(book_datas[-1])
+
         if len(book_datas[2]) > 35:
             book_datas[2] = book_datas[2][:35]
 
-        with open(os.path.join(SAVE_PATH, "books_images", f"{book_datas[7]}", f"{book_datas[2]}.jpg"), "wb") as image:
+        with open(os.path.join(SAVE_PATH, "books_images", f"{book_datas[7]}",
+                               f"{book_datas[2]}.jpg"), "wb") as image:
             image.write(response.content)
 
+    category_books_datas.append(book_datas)
+    return category_books_datas
 
-scrap_images = menu()
-categories_urls_list = get_categories()
-get_soup(scrap_images, categories_urls_list)
+
+def create_csv(category_books_datas):
+    """ create csv output file with date & hour """
+
+    category_path = f"{SAVE_PATH}/extracted_data_{TODAY_DATE}/{category_books_datas[0][7]}"
+
+    # Check if directory already exists before writing
+    if not os.path.exists(category_path):
+        os.makedirs(category_path)
+
+    lenght_category_books = len(category_books_datas)
+
+    with open(os.path.join(category_path, "Output.csv"), "w", newline="", encoding="utf-8") as output:
+        write = csv.writer(output)
+        write.writerow(CSV_HEADER)
+
+        for i in range(lenght_category_books):
+            write.writerow(category_books_datas[i])
+
+
+while True:
+    menu_choice = input("Want to get images ? y/n ")
+    if menu_choice == "n":
+        scrap_images = False
+        break
+    elif menu_choice == "y":
+        scrap_images = True
+        break
+
+start_time = time.time()
+
+get_categories()
