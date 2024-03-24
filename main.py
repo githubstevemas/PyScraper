6d5b583396d2163c17732202d2a1e535d0ace1b1
Steve <mas.ste@gmail.com>
2024-03-24 10:57:18 +0100

    Refactoring

--- main.py
+++ main.py
@@ -9,75 +9,69 @@ URL_DOMAIN = "https://books.toscrape.com"
 CSV_HEADER = ["product_page_url", "universal_ product_code (upc)", "title", "price_including_tax",
               "price_excluding_tax", "number_available", "product_description", "category", "review_rating",
               "image_url"]
-SAVE_PATH = "Outputs"
 TODAY_DATE = datetime.now().strftime("%d_%m_%Y")
+SAVE_PATH = f"Outputs/extracted_data_{TODAY_DATE}"
 
 
 def get_categories():
-    """ from domain url, get a list of categories urls """
+    """ from domain url get a list of categories urls """
 
     print("Scraping in progress...")
     response = requests.get(URL_DOMAIN)
-    categories_soup = BeautifulSoup(response.text, "html.parser")
+    main_soup = BeautifulSoup(response.text, "html.parser")
+
+    create_directories(main_soup)
 
     categories_list = []
-    for a in categories_soup.find_all("a"):
+    for a in main_soup.find_all("a"):
         categories_list.append(a["href"])
 
     for category in categories_list:
-
         if "category/books/" in category:
-            urls_category_list = []
-
             url_category = "https://books.toscrape.com/" + category
-            urls_category_list.append(url_category)
 
             category_books_datas = get_categories_soups(url_category)
             create_csv(category_books_datas)
 
-    print(f" {URL_DOMAIN} succefully scraped.")
+    print(f"{URL_DOMAIN} succefully scraped.")
 
 
 def get_categories_soups(url_category):
+    """ from category url get his soup and check if next page is present """
+
     next_page_number = 1
-    categories_soups = []
+    category_page_datas = []
 
-    while True:
-        try:
-            responses = requests.get(url_category)
-            cat_soup = BeautifulSoup(responses.text, "html.parser")
-            categories_soups.append(cat_soup)
+    while url_category:
+        response = requests.get(url_category)
+        cat_soup = BeautifulSoup(response.text, "html.parser")
 
-            next_page_number += 1
-            next_link = cat_soup.find("li", class_="next")
-            next_url = next_link.rsplit("/", 1)[0] + f"page-{next_page_number}.html"
+        category_books_datas = get_books_category(cat_soup, category_page_datas)
 
-            url_category = next_url
-        except:
+        next_page_number += 1
+        if cat_soup.find("li", class_="next"):
+            url_category = url_category.rsplit("/", 1)[0] + f"/page-{next_page_number}.html"
+        else:
             break
 
-    category_books_datas = get_books_category(categories_soups)
     return category_books_datas
 
 
-def get_books_category(categories_soups):
+def get_books_category(cat_soup, category_page_datas):
     """ from category url get books of all the category pages """
 
-    category_books_datas = []
-    for i in range(len(categories_soups)):
-        liste_h3 = categories_soups[i].find_all("h3")
+    h3_list = cat_soup.find_all("h3")
 
-        for h3 in liste_h3:
-            a_h3 = h3.find("a")
-            book_url = "https://books.toscrape.com/catalogue" + a_h3.get("href").replace("../../..", "")
+    for h3 in h3_list:
+        book_url = "https://books.toscrape.com/catalogue" + h3.find("a")["href"].replace("../../..", "")
 
-            response = requests.get(book_url)
-            soup_book = BeautifulSoup(response.text, "html.parser")
+        response = requests.get(book_url)
+        soup_book = BeautifulSoup(response.text, "html.parser")
 
-            book_datas = get_infos(book_url, soup_book)
-            category_books_datas.append(book_datas)
+        book_datas = get_infos(book_url, soup_book)
+        category_page_datas.append(book_datas)
 
-    return category_books_datas
+    return category_page_datas
 
 
 def get_infos(book_url, soup_book):
@@ -105,8 +99,7 @@ def get_infos(book_url, soup_book):
     else:
         reviews = 5
 
-    image = soup_book.find("img")
-    url_image = URL_DOMAIN + image["src"].replace("../..", "")
+    url_image = URL_DOMAIN + soup_book.find("img")["src"].replace("../..", "")
 
     if soup_book.find("p", class_=""):
         description = soup_book.find("p", class_="").text
@@ -117,7 +110,6 @@ def get_infos(book_url, soup_book):
     category_name = category_ul.find_all("a")[2].text
 
     li_title = category_ul.find_all("li")[3].text
-
     title = re.sub(r'[\\/*?:"<>|]', "", li_title)
 
     book_datas = [book_url, upc, title, tax_price, price, count, description, category_name, reviews,
@@ -129,17 +121,29 @@ def get_infos(book_url, soup_book):
     return book_datas
 
 
-def get_images(book_datas):
+def create_directories(main_soup):
+    """ from main page create directories for images and csv outputs """
 
-    if not os.path.exists(os.path.join(SAVE_PATH, "books_images", f"{book_datas[7]}")):
-        os.makedirs(os.path.join(SAVE_PATH, "books_images", f"{book_datas[7]}"))
+    if not os.path.exists(f"Outputs/extracted_data_{TODAY_DATE}"):
+        os.makedirs(f"Outputs/extracted_data_{TODAY_DATE}")
+
+    if scrap_images:
+        categories = main_soup.find("ul", class_="nav nav-list")
+
+        for i in categories.find_all("li"):
+            category_name = i.find("a").text.strip()
+            os.makedirs(os.path.join("Outputs", "books_images", f"{category_name}"))
+
+
+def get_images(book_datas):
+    """ from infos book write images on local path """
 
     response = requests.get(book_datas[-1])
 
     if len(book_datas[2]) > 35:
         book_datas[2] = book_datas[2][:35]
 
-    with open(os.path.join(SAVE_PATH, "books_images", f"{book_datas[7]}",
+    with open(os.path.join("Outputs", "books_images", f"{book_datas[7]}",
                            f"{book_datas[2]}.jpg"), "wb") as image:
         image.write(response.content)
 
@@ -147,12 +151,7 @@ def get_images(book_datas):
 def create_csv(category_books_datas):
     """ create csv output file with date & hour """
 
-    category_path = f"{SAVE_PATH}/extracted_data_{TODAY_DATE}"
-
-    if not os.path.exists(category_path):
-        os.makedirs(category_path)
-
-    with open(os.path.join(category_path, f"Output - {category_books_datas[0][7]}.csv"),
+    with open(os.path.join(SAVE_PATH, f"Output - {category_books_datas[0][7]}.csv"),
               "w", newline="", encoding="utf-8") as output:
         write = csv.writer(output)
         write.writerow(CSV_HEADER)
